5888ae5cc7d6f1f4d89fa687242c03b94679837b
Vatsal <118737969+balasravatsal@users.noreply.github.com>
2023-06-05 10:14:57 +0000

    API calling

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import { useQueries, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const url =
+    `https://api.unsplash.com/search/photos/?client_id=${import.meta.env.VITE_API_KEY}`;
 
 const Gallery = () => {
+    const { searchTerm } = useGlobalContext();
+
+    const response = useQuery({
+        queryKey: ["images", searchTerm],
+        queryFn: async () => {
+            const result = await axios.get(`${url}&query=${searchTerm}`);
+            return result.data;
+        },
+    });
+
+    // console.log(response);
+    if (!searchTerm) {
+        return (
+            <section className='image-container'>
+                <h2>Please enter the keywords</h2>
+            </section>
+        );
+    }
+
+    if (response.isLoading) {
+        return (
+            <section className="image-container">
+                <h4>Loading...</h4>
+            </section>
+        );
+    }
+
+    if (response.isError) {
+        return (
+            <section className="image-container">
+                <h4>There was an error!</h4>
+            </section>
+        );
+    }
+
+    const results = response.data.results;
+
+    if (results.length < 1) {
+        return (
+            <section className="image-container">
+                <h4>No result found</h4>
+                <p>Try something different</p>
+            </section>
+        );
+    }
+
     return (
-        <div>
-            img
-        </div>
+        <section className="image-container">
+            {results.map((item) => {
+                const imgURL = item?.urls?.regular;
+                return (
+                    <img
+                        src={imgURL}
+                        key={item.id}
+                        alt={item.alt_description}
+                        className="img"></img>
+                );
+            })}
+        </section>
     );
 };
 
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
+
+    const {setSearchTerm} = useGlobalContext()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const searchValue = e.target.elements.search.value
+        if (!searchValue) return;
+        console.log(searchValue);
+        setSearchTerm(() => searchValue) 
+    };
+
     return (
-        <div>
-          form
-        </div>
+        <section>
+            <h1 className="title">Image Fetcher</h1>
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Enter keyword"
+                    className="form-input search-input"
+                />
+                <button type="submit" className="btn">
+                    Search
+                </button>
+            </form>
+        </section>
     );
 };
 
